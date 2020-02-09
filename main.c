bc2dcd2f4a6c6a4637b0405d82fda37da5fe312c
Hector Soto Jr <59711305+SpeedySoto@users.noreply.github.com>
2020-02-09 14:19:02 -0800

    Lab 8 Exercise 1

--- main.c
+++ main.c
@@ -1,37 +1,37 @@
 
 /*	Partner(s) Name & E-mail: David Wilson
  *	Lab Section: 023
- *	Assignment: Lab #8  Exercise #2 
+ *	Assignment: Lab #8  Exercise #1 
  *	Exercise Description: [optional - include for your own benefit]
  *	
  *	I acknowledge all content contained herein, excluding template 
  * 	or example code, is my own original work.
  */
 
+
 #include <avr/io.h>
-#include <avr/interrupt.h>
 
 void ADC_init() {
 	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
 
 }
 
-
-
 int main(void)
 {
-	DDRA = 0x00; PORTA = 0xFF; 
+
 	DDRB = 0xFF; PORTB = 0x00;
-	DDRD = 0xFF; PORTD = 0x00; 
-
+	DDRD = 0xFF; PORTD = 0x00;
+	
 	ADC_init();
-	while(1){
-		unsigned short x = ~ADC; 
-		unsigned char one8 = (char)x;
-		PORTB = one8;
-		unsigned char Back2 = (char)(x >> 8);
-		PORTD = Back2;
-
+	
+	unsigned short A2D = ADC;
+	//unsigned char my_char = (char)my_short; // my_char = 0xCD
+	//my_char = (char)(my_short >> 4); // my_char = 0xBC
+	
+	while (1)
+	{
+		PORTB = (char) A2D;
+		PORTD = (char) (A2D >> 4);
+		A2D = ADC;
 	}
 }
-
